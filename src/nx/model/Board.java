0d045fe7f667728a82cda48cd15b153e90a0341d
Miles <Miles@Softie.lan>
2017-09-30 21:27:42 +1000

    Incremental commit

--- src/nx/model/Board.java
+++ src/nx/model/Board.java
@@ -5,5 +5,26 @@ package nx.model;
  */
 
 public class Board {
-
+	public static final int MAX_ROW = 3;
+	public static final int MAX_COLUMN = 3;
+	
+	private final Piece board[][] = {
+			{Piece.None,Piece.None,Piece.None},
+			{Piece.None,Piece.None,Piece.None},
+			{Piece.None,Piece.None,Piece.None}
+			};
+	
+	public boolean place(int x, int y, Piece piece) {
+		if(Piece.None == board[x][y] && Piece.None != piece) {
+			board[x][y] = piece;
+			return true;
+		}
+		return false;
+	}
+	
+	public Piece get(int x, int y) {
+		return board[x][y];
+	}
+	
+	
 }
